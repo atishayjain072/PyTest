f6e9eb190c3a009062f438b81b7a228d0fa518b9
atishayjain072 <jain.atishay072@gmail.com>
2023-09-18 10:23:14 +0530

    PyTest

---
+++ PyTest.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import os
+l1 = [1,2,3,4,5]
+l1.reverse()
+print("Reverse List", l1)
+
+l2 = [1,2,3,4,5]
+l2.sort()
+print("Sorted list", l2)
+
+l3 = []
+l3 = l1.copy()
+print("After Copying list", l3)
+
+index = l2[2:6]
+print("The index value are", index)
+
